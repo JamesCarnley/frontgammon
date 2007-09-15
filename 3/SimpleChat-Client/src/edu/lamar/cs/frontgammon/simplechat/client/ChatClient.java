179594fc74ce853e082d103cd344641b392c8022
JamesCarnley <JamesCarnley@a33b19a4-7939-0410-bf54-9ffe9c5c0391>
2007-09-15 20:46:33 +0000

    E50 completed

--- 3/SimpleChat-Client/src/edu/lamar/cs/frontgammon/simplechat/client/ChatClient.java
+++ 3/SimpleChat-Client/src/edu/lamar/cs/frontgammon/simplechat/client/ChatClient.java
@@ -70,17 +70,100 @@ public class ChatClient extends AbstractClient {
      * @param message The message from the UI.
      */
     public void handleMessageFromClientUI(String message) {
-        try {
-            sendToServer(message);
-        } catch(IOException e) {
-            clientUI.display
-                    ("Could not send message to server.  Terminating client.");
-            quit();
+        
+        // Case doesn't matter
+        message = message.toLowerCase();
+        
+        // All messages starting with # are commands
+        if (message.startsWith("#")) {
+            
+            /* This process of parsing commands uses nested if elses.
+             * There is really no clean way of doing this, so each if statement
+             * has a slightly hidden corresponding else. This allows the command
+             * processing to cascade and if none of the if statements catch then 
+             * we find that the command is an error and we tell the user so.
+            */
+            
+            String command = message.substring(1);
+            
+            if (command.equals("quit"))
+                quit();
+            else
+            
+            if (command.equals("logoff")) {
+                try {
+                    closeConnection();
+                } catch (IOException ex) {
+                    clientUI.display("Could not logoff of server: " + ex);
+                }
+            } else
+            
+            if (command.startsWith("sethost")) {
+                String[] commandParts = command.split(" ");
+                String host = commandParts[1];
+                
+                if (this.isConnected() == false)
+                {
+                    clientUI.display("Host set to " + host);
+                    setHost(host);
+                } else {
+                    clientUI.display("Could not set host. You must log off of the current server first.");
+                }
+            } else
+            
+            if (command.startsWith("setport")) {
+                String[] commandParts = command.split(" ");
+                if (this.isConnected() == false)
+                {
+                    try {
+                        int port = Integer.parseInt(commandParts[1]);
+                        clientUI.display("Port set to " + port);
+                        setPort(port);
+                    } catch (NumberFormatException ex) {
+                        clientUI.display("Setting port failed: Port was not an integer.");
+                    }
+                } else {
+                    clientUI.display("Could not set port number. You must log off of the current server first.");
+                }
+            } else
+            
+            if (command.equals("login"))
+            {
+                if (isConnected() == false)
+                {
+                    try {
+                        openConnection();
+                    } catch (IOException ex) {
+                        clientUI.display("Could not login to server: " + ex);
+                    }
+                } else {
+                    clientUI.display("You must disconnect from the current server before attempting to connect to another.");
+                }
+            } else
+            
+            if (command.equals("gethost"))
+                clientUI.display(getHost());
+            else
+            
+            if (command.equals("getport"))
+                clientUI.display(Integer.toString(getPort()));
+            else
+            {
+                clientUI.display("Invalid command.");
+            }
+            
+        } else {
+            try {
+                sendToServer(message);
+            } catch(IOException e) {
+                clientUI.display("Could not send message to server.  Terminating client.");
+                quit();
+            }
         }
     }
     
     // E49 - Responding to server shutdown
-    protected void connectionException (Exception exception) {
+    protected void connectionException(Exception exception) {
         System.out.println("The server has shut down. The client will now quit.");
         quit();
     }
