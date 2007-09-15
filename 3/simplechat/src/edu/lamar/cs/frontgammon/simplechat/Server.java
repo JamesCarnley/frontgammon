56ee25c78022175c7eac769ee80db9f8d48a21bd
JamesCarnley <JamesCarnley@a33b19a4-7939-0410-bf54-9ffe9c5c0391>
2007-09-15 13:44:22 +0000

    Initial files

---
+++ 3/simplechat/src/edu/lamar/cs/frontgammon/simplechat/Server.java
@@ -0,0 +1,22 @@
+/*
+ * Server.java
+ *
+ * Created on September 15, 2007, 8:36 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.lamar.cs.frontgammon.simplechat;
+
+/**
+ *
+ * @author James
+ */
+public class Server {
+    
+    /** Creates a new instance of Server */
+    public Server() {
+    }
+    
+}
---
+++ 3/simplechat/src/edu/lamar/cs/frontgammon/simplechat/SimpleChat.java
@@ -0,0 +1,29 @@
+/*
+ * SimpleChat.java
+ *
+ * Created on September 15, 2007, 8:36 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package edu.lamar.cs.frontgammon.simplechat;
+
+import com.lloseng.ocsf.client.AbstractClient;
+
+/**
+ *
+ * @author James
+ */
+public class SimpleChat {
+    
+    /** Creates a new instance of SimpleChat */
+    public SimpleChat() {
+    }
+    
+    public static void main()
+    {
+        System.out.println("Starting SimpleChat...");
+        System.out.println("Exitting SimpleChat...");
+    }
+}
